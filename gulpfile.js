95b35d4f10b9b5c708595c295f9fb594dd11094a
zhenzhen <@2868382033.com>
2018-12-04 18:39:39 +0800

    编译css完成

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+let gulp = require("gulp");
+let sass = require("gulp-sass");
+let concat = require("gulp-concat");
+let minCss = require("gulp-clean-css")
+let babel = require("gulp-babel");
+let htmlmin = require("gulp-htmlmin");
+let autoprefixer = require("gulp-autoprefixer");
+let server = require("gulp-webserver")
+let uglify = require("gulp-uglify")
+let path = require("path")
+let url = require("url");
+let fs = require("fs")
+
+gulp.task("devscss", function() {
+    return gulp.src("./src/scss/*.scss")
+        .pipe(sass())
+        .pipe(concat('all.css'))
+        .pipe(minCss())
+        .pipe(gulp.dest("./src/css"))
+
+})
+
+
+gulp.task('server', function() {
+    return gulp.src('src')
+        .pipe(server({
+            port: 3030,
+            open: true,
+            livereload: true,
+            middleware: function(req, res, next) {
+                var pathname = url.parse(req.url).pathname;
+                if (pathname === '/favicon.ico') {
+                    res.end()
+                    return
+                }
+                if (pathname === '/api/list') {
+                    res.end(JSON.stringify({ code: 1, data: list }))
+                } else {
+                    pathname = pathname === '/' ? 'index.html' : pathname;
+                    console.log(pathname);
+                    res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)));
+                }
+            }
+        }))
+})
+gulp.task("bUglify", function() {
+    return gulp.src(["./src/js/*.js"])
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest("./bulid/js"))
+})
+
+
+gulp.task("bHtml", function() {
+    return gulp.src("./src/**/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('./bulid'))
+
+})
+
+gulp.task("watch", function() {
+    gulp.watch("./src/scss/*.scss", gulp.series("devscss"))
+    gulp.watch("./src/js/*.js", gulp.series("bUglify"))
+})
+gulp.task("defalut", gulp.series("bUglify", "copyLibs", "watch"))
